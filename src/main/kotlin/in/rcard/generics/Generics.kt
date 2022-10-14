82a3e391fac5a149bdeea9a037a99af7515a61b6
rcardin <riccardo.cardin@lastminute.com>
2022-10-14 12:20:25 +0200

    Added some tricks on type parameters declaration in Kotlin generics

---
+++ src/main/kotlin/in/rcard/generics/Generics.kt
@@ -0,0 +1,18 @@
+package `in`.rcard.generics
+
+object Generics {
+
+    // Generic with a type parameter not allowing null values
+    class Processor<T : Any> {
+        fun process(value: T) {
+            value.hashCode()
+        }
+    }
+
+    // Generic with more than one type parameter constraint
+    class Processor2<T> where T : Comparable<T>, T : Appendable {
+        fun process(value: T) {
+            value.append("!")
+        }
+    }
+}
